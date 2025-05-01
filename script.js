9f392a8be9777cb33a4294ea390aa2925d6ff92e
Giampier Aliaga Esquivel <usuarioae87@gmail.com>
2025-05-01 13:38:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Mostrar lo que hace el script
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = 'Este script muestra el contenido de la sección Cuaderno y realiza otras tareas interactivas si es necesario.';
+});
 
